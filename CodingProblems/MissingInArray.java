44c0b3adc4a2c416125f1df65e0d85acac8fbc9f
VaishnaviKenchi <vkenchi5@gmail.com>
2026-03-26 13:29:25 +0530

    Update MissingInArray.java

--- CodingProblems/MissingInArray.java
+++ CodingProblems/MissingInArray.java
@@ -3,17 +3,17 @@ public class MissingInArray {
     public static int findMissingElement(int[] arr)
     {
         int n=arr.length+1;  
-
         int total=n*(n+1)/2;
-
         int sum=0;
+        
         for(int num:arr)
         {
             sum=sum+num;
         }
         return total-sum;
     }
-    public static void main(String args[]){
+    public static void main(String args[])
+    {
         int arr[]={3,5,2,7};
         System.out.println(findMissingElement(arr));
     }
